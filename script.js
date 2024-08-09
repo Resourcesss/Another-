939ab47b7777865018a03382971358f769d3c755
Resourcesss <Segunpraise47@gmail.com>
2024-08-09 21:21:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-window.addEventListener('load', function() {
-    // Fade in the page content
-    document.body.style.opacity = '1';
-    
-    // Fade out the logo
-    const fadeImageContainer = document.getElementById('fadeImageContainer');
-    fadeImageContainer.style.opacity = '1';
-    fadeImageContainer.style.transition = 'opacity 3s';
-    setTimeout(() => {
-        fadeImageContainer.style.opacity = '0';
-    }, 100);
-});
-
 document.getElementById('detailsForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
